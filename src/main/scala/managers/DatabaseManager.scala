3b701845523e8eb17adeaea92c0ad4f85818e2aa
Kamil Radziszewski <errhal@gmail.com>
2017-10-31 18:35:09 +0100

    Added simple authentication.

--- src/main/scala/managers/DatabaseManager.scala
+++ src/main/scala/managers/DatabaseManager.scala
@@ -1,6 +1,22 @@
 package managers
 
+import java.io.File
+
+import com.fasterxml.jackson.databind.{JsonNode, ObjectMapper}
+import config.ConfigurationProvider
+
 object DatabaseManager {
+
+  def saveData(datafile:String, data: Any): Unit = {
+
+  }
+
+  def loadData(datafile:String): JsonNode = {
+    val dbFile = new File(ConfigurationProvider.getDbLocation() + "/" + datafile + ".db")
+    val objectMapper = new ObjectMapper()
+    val jsonNode = objectMapper.readValue(dbFile, classOf[JsonNode])
+    jsonNode
+  }
 
 
 
--- src/main/scala/services/AuthenticationService.scala
+++ src/main/scala/services/AuthenticationService.scala
@@ -1,9 +1,18 @@
 package services
 
+import managers.DatabaseManager
+
 object AuthenticationService {
 
-  def authenticate(message: String): Unit = {
+  def authenticate(message: String): Boolean = {
+    val accounts = DatabaseManager.loadData("accounts").fields
+    val credentials = message.split("\\[").flatMap(s => s.split("\\]")).apply(1).split(":")
 
+    while (accounts.hasNext) {
+      val account = accounts.next
+      return account.getKey == credentials(0) && account.getValue.asText() == credentials(1)
+    }
+    false
   }
 
 }
