9a64bd5d2f6af72e91af4e383aa246da90e88506
Kamil Radziszewski <errhal@gmail.com>
2018-01-04 23:28:55 +0100

    Simple stress test for KV select statement.

---
+++ src/test/scala/stress/SelectStressTest.scala
@@ -0,0 +1,48 @@
+package stress
+
+import java.io.{BufferedReader, InputStreamReader, PrintWriter}
+import java.net.Socket
+
+import scala.concurrent.{ExecutionContext, Future}
+import scala.util.{Failure, Success}
+
+object SelectStressTest {
+
+  val MAX_TIMEOUT = 3000
+
+  def main(args: Array[String]) {
+
+    implicit val ec = ExecutionContext.global
+
+    val futures = List.empty[Future[Any]]
+
+    val startTime = System.nanoTime()
+
+    for (i <- 1 to 1000) {
+      futures.:+(Future {
+        val socket = new Socket("127.0.0.1", 7000)
+        val reader = new BufferedReader(new InputStreamReader(socket.getInputStream))
+        val printWriter = new PrintWriter(socket.getOutputStream, true)
+        printWriter.println("query[select key(key1) from db1]")
+        var r = reader.readLine()
+        while (r != null) {
+          r = reader.readLine()
+        }
+      })
+    }
+
+    val seqFutures = Future.sequence(futures)
+    seqFutures.onComplete({
+      case Success(_) => {
+        val endTime = System.nanoTime()
+        println("Stress test finished in " + (endTime - startTime) / 1000000 + "ms.")
+      }
+      case Failure(e) => {
+        println(e)
+      }
+    })
+
+    Thread.sleep(MAX_TIMEOUT)
+  }
+
+}
